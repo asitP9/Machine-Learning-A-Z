9c1bb4746aa4b10c187447b129a63dfb28d28122
AsitPanda-at-100487626160 <panda9asit@gmail.com>
2020-09-06 15:48:53 +0400

    Using BackPropagation Elimination, decreasing the feature for multiple Regression

---
+++ Part 2 Regression/Section 3 Polynomial Regression/temp.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+dataset=pd.read_csv('50_Startups.csv')
+
+X=dataset.iloc[:, :-1].values
+Y=dataset.iloc[:, 4].values
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+onehotencoder=OneHotEncoder(categories='auto')
+column_transformer=ColumnTransformer([('one_hot_encoder', onehotencoder, [3])], remainder='passthrough')
+X=column_transformer.fit_transform(X)
+
+X=X[:, 1:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test= train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train, Y_train) 
+
+y_pred=regressor.predict(X_test)
+
+import statsmodels.api as sm
+X=np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+
+
+X_opt=np.array(X[:, [0, 1, 2, 3, 4, 5]], dtype=float)
+columnsInX_Opt=np.array([0, 1, 2, 3, 4, 5])
+xOpt_Final=False
+
+while (xOpt_Final==False):
+    regressor_ols=sm.OLS(endog=Y, exog=X_opt).fit()
+    max=regressor_ols.pvalues[0]
+    finalindex=0
+    # xOpt_Final=True
+
+    for index, value in np.ndenumerate(columnsInX_Opt):
+        # print(i)
+        if regressor_ols.pvalues[index]>max:
+            max=regressor_ols.pvalues[index]
+            finalIndex=index
+    
+    if max>0.05:
+        print("finalIndex ")
+        print(finalIndex)
+        X_opt=np.delete(X_opt, np.s_[finalIndex], axis=1) 
+        # X_opt=np.delete(X_opt, X_opt[finalIndex], axis=1)
+        # print("X_OPT")
+        # print(X_opt)
+        columnsInX_Opt=np.delete(columnsInX_Opt, [finalIndex])
+        # columnsInX_Opt=columnsInX_Opt[columnsInX_Opt!=finalIndex]
+    
+    elif max<=0.05:
+        xOpt_Final=True
+        print(columnsInX_Opt)
+        xOpt_Final=True
+        # print(regressor_ols.pvalues[i])
